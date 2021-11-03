63ff0342858f7493255e0a8a17a4696a5a79c234
hochunlin28 <hochunlin28@MSI.localdomain>
2021-11-03 16:44:46 +0800

    add main.py and inference.py

---
+++ inference.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas as pd
+import os
+import matplotlib.image as mpimg
+
+import torch
+import torch.nn as nn
+import torch.optim as optim 
+
+import torchvision
+from torch.utils.data import DataLoader, Dataset
+import torch.utils.data as utils
+from torchvision import transforms
+
+import matplotlib.pyplot as plt
+from PIL import Image
+
+#use pandas to read the all classes
+table = pd.read_csv("./classes.txt",header = None)
+print(table)
+
+#load model and evaluate
+model = torch.load("./savedModel.pt")
+model.eval()
+
+with open('testing_img_order.txt') as f:
+     test_images = [x.strip() for x in f.readlines()]  # all the testing images
+
+''' 
+data_transf = transforms.Compose([
+transforms.RandomHorizontalFlip(p=0.5),
+transforms.RandomRotation([-15,15], expand=False, center = (112,112)), 
+transforms.CenterCrop([350, 300]),
+transforms.Resize([224,224]),
+transforms.ToTensor()])
+'''    
+data_transf = transforms.Compose([transforms.CenterCrop([350, 300]),
+transforms.Resize([224,224]),
+transforms.ToTensor()])
+
+submission = []
+for img_dir in test_images:  # image order is important to your result
+    img = Image.open("./test/"+img_dir)
+    img = data_transf(img)
+    img = img.unsqueeze(0) #from 3 dimension (3*224*224) to four dimension (1*3*224*224)
+    if torch.cuda.is_available():
+        img = img.cuda()
+    predicted_class = model(img)  # the predicted category
+    n = nn.Softmax(dim=1) # use softmax to easily recognize answer
+    predicted_class = n(predicted_class)
+    print(predicted_class.max(1)[1].cpu().item()) # print predicted index
+    submission.append([img_dir, table[0][predicted_class.max(1)[1].cpu().item()]])
+
+#save answer
+np.savetxt('answer.txt', submission, fmt='%s')
---
+++ main.py
@@ -0,0 +1,129 @@
+'''
+hyperparameter setting:
+    model : efficientnet
+    learing rate: 0.0002
+    optimizer: Adam
+    loss function: cross entropy loss
+    batch size: 22
+    epoch: 50
+'''
+import numpy as np
+import pandas as pd
+import os
+import matplotlib.image as mpimg
+
+import torch
+import torch.nn as nn
+import torch.optim as optim 
+
+import torchvision
+from torch.utils.data import DataLoader, Dataset
+import torch.utils.data as utils
+from torchvision import transforms
+
+import matplotlib.pyplot as plt
+import pandas as pd
+from efficientnet_pytorch import EfficientNet
+from PIL import Image
+
+data_dir = './'
+train_dir = data_dir + 'train/'
+test_dir = data_dir + 'test/'
+
+#use pandas to read the training labels
+labels = pd.read_csv('training_labels.txt', sep=" ",header=None)
+labels.columns = ["id", "bird_class"]
+
+#class ImgaeData to get labels and transformed images
+class ImageData(Dataset):
+    def __init__(self, df, data_dir, transform):
+        super().__init__()
+        self.df = df
+        self.data_dir = data_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.df)
+    
+    def __getitem__(self, index):       
+        img_name = self.df.id[index]
+        number = self.df.bird_class[index]
+        number = int(number[:number.index(".")]) - 1
+        label = number
+        img_path = os.path.join(self.data_dir, img_name)
+        image = Image.open(img_path)
+        image = self.transform(image)
+
+        return image, label
+
+#use dataloader to generate training dataset
+data_augmentation = transforms.RandomChoice([
+transforms.RandomHorizontalFlip(p=0.5),
+transforms.RandomRotation([-15,15], expand=True),
+transforms.CenterCrop([350, 310])])
+
+data_transf = transforms.Compose([
+data_augmentation,
+transforms.Resize([224,224]),
+transforms.ToTensor()])
+
+'''
+data_transf = transforms.Compose([
+transforms.RandomHorizontalFlip(p=0.5),
+transforms.RandomRotation([-15,15], expand=True), 
+transforms.CenterCrop([350, 310]),
+transforms.Resize([224,224]),
+transforms.ToTensor()])
+'''
+
+train_data = ImageData(df = labels, data_dir = train_dir, transform = data_transf)
+train_loader = DataLoader(dataset = train_data, batch_size = 22, shuffle = True)
+
+#load model from pretrained
+model = EfficientNet.from_pretrained('efficientnet-b1')
+#model = torch.load("./save.pt")
+print("load_success")
+
+#unfreeze model weights
+for param in model.parameters():
+    param.requires_grad = True
+
+#change the last fully connected layer to fit the 200 labels
+feature = model._fc.in_features
+model._fc = nn.Linear(in_features=feature,out_features=200,bias=True)
+
+#use GPU to train
+model = model.to('cuda')
+
+#set optimize function and loss function
+optimizer = optim.Adam(model.parameters(),lr = 0.0001)
+loss_func = nn.CrossEntropyLoss(label_smoothing=0.1)
+
+# Train model
+loss_log = []
+
+
+for epoch in range(50):    
+    model.train()
+    train_correct = 0    
+    for ii, (data, target) in enumerate(train_loader):
+        #target = np.array(target).astype(float)
+        #target = torch.from_numpy(target)
+        #target = target.float()                
+
+        data, target = data.cuda(), target.cuda()
+        optimizer.zero_grad()
+        output = model(data)
+        loss = loss_func(output, target)
+        loss.backward()
+        train_correct += (output.max(1)[1] == target).sum()
+        optimizer.step()  
+        if ii % 50 == 0:
+            print("batch: %s" %ii)
+        if ii % 1000 == 0:
+            loss_log.append(loss.item())
+       
+    print('Epoch: {} - Loss: {:.6f} - Correct: {}'.format(epoch + 1, loss.item(), train_correct))
+
+torch.save(model,"save.pt")
+
